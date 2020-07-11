c746e7e8360c1140d9fc52130c91140e2fe7a8af
andr3slelouch <luis.andradec14@gmail.com>
2020-07-11 10:55:09 -0500

    Better histogram with opencv

--- src/main.py
+++ src/main.py
@@ -113,7 +113,12 @@ class Application(tk.Frame):
         image = self.image
         if len(image.shape) == 3:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        plt.hist(image.ravel(),256,[0,256]) 
+        #x=image[:,:,0]
+        histr = cv2.calcHist([image],[0],None,[256],[0,256]) 
+        plt.title("HIstogramm for given Image")
+        plt.xlabel("Value")
+        plt.ylabel("pixels Frequency")
+        plt.plot(histr) 
         plt.show() 
 
     def apply_filter(self):
